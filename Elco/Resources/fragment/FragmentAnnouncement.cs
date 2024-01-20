6344322c25074564ece0bf1de76e6134842121c2
codingKlyde <>
2024-01-21 00:05:23 +0800

    List preparation

--- Elco/Resources/fragment/FragmentAnnouncement.cs
+++ Elco/Resources/fragment/FragmentAnnouncement.cs
@@ -1,13 +1,33 @@
 ﻿using Android.OS;
 using Android.Views;
+using Android.Widget;
+using Elco.Adapter;
+using System.Collections.Generic;
 
 namespace Elco
 {
     public class FragmentAnnouncement : AndroidX.Fragment.App.Fragment
     {
+        private List<string> nItems;
+        private ListView mListView;
+
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            return inflater.Inflate(Resource.Layout.activity_announcement, container, false);
+            View view = inflater.Inflate(Resource.Layout.activity_announcement, container, false);
+
+            mListView = view.FindViewById<ListView>(Resource.Id.listView_announcement);
+
+            nItems = new List<string>();
+            nItems.Add("Announcement 1");
+            nItems.Add("Announcement 2");
+            nItems.Add("Announcement 3");
+
+
+            AnnouncementAdapter adapter = new AnnouncementAdapter(Activity, nItems);
+            mListView.Adapter = adapter;
+
+
+            return view;
         }
     }
 }
