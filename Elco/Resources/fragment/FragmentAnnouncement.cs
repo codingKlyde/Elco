ff29448e9b6d4546d5647f9959945f8d29c3ad43
codingKlyde <>
2024-01-22 08:16:45 +0800

    Better list design and logic

--- Elco/Resources/fragment/FragmentAnnouncement.cs
+++ Elco/Resources/fragment/FragmentAnnouncement.cs
@@ -2,13 +2,16 @@
 using Android.Views;
 using Android.Widget;
 using Elco.Adapter;
+using Elco.Model;
+using System;
 using System.Collections.Generic;
+using static Android.Content.ClipData;
 
 namespace Elco
 {
     public class FragmentAnnouncement : AndroidX.Fragment.App.Fragment
     {
-        private List<string> nItems;
+        private List<ModelAnnouncement> nItems;
         private ListView mListView;
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -17,17 +20,32 @@ namespace Elco
 
             mListView = view.FindViewById<ListView>(Resource.Id.listView_announcement);
 
-            nItems = new List<string>();
-            nItems.Add("Announcement 1");
-            nItems.Add("Announcement 2");
-            nItems.Add("Announcement 3");
+            nItems = new List<ModelAnnouncement>();
 
+            nItems.Add(new ModelAnnouncement() { 
+                announcementDate = "Feb. 5, 2024", 
+                announcementDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent odio nibh, mollis vitae luctus ac, fermentum quis eros. Fusce pharetra aliquet felis id vestibulum. Integer diam urna, dapibus nec pellentesque at, iaculis sit amet arcu. Mauris et magna a enim posuere imperdiet. In bibendum orci at feugiat finibus. Sed a.",
+                announcementTime = "1 pm - 5 pm", 
+                announcementTitle = "Power Interruption" 
+            });
+            nItems.Add(new ModelAnnouncement()
+            {
+                announcementDate = "Sept. 21, 2024",
+                announcementDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent odio nibh, mollis vitae luctus ac, fermentum quis eros. Fusce pharetra aliquet felis id vestibulum. Integer diam urna, dapibus nec pellentesque at, iaculis sit amet arcu. Mauris et magna a enim posuere imperdiet. In bibendum orci at feugiat finibus. Sed a.",
+                announcementTime = "7 am - 11 am",
+                announcementTitle = "Power Interruption"
+            });
 
-            AnnouncementAdapter adapter = new AnnouncementAdapter(Activity, nItems);
+            AdapterAnnouncement adapter = new AdapterAnnouncement(Activity, nItems);
             mListView.Adapter = adapter;
-
+            mListView.ItemClick += itemClick;
 
             return view;
+        }
+
+        private void itemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
