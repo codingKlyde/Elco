a454dea13bae89005ad5ee791c05ad3e720c173f
codingKlyde <>
2024-01-22 08:17:30 +0800

    Add class for announcement item parts

---
+++ Elco/Model/ModelAnnouncement.cs
@@ -0,0 +1,12 @@
+﻿namespace Elco.Model
+{
+    class ModelAnnouncement
+    {
+        public string announcementDate { get; set; }
+        public string announcementDescription { get; set; }
+        public string announcementAffectedArea { get; set; }
+        public string announcementImage { get; set; }
+        public string announcementTitle { get; set; }
+        public string announcementTime { get; set; }
+    }
+}
